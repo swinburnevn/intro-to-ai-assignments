253862b8130ecefb57446c7108e474e9987c8042
Mikanwolfe <ivory@nekox.net>
2020-03-10 13:48:03 +1100

    update

---
+++ tutorial-2/tutorial-2/tutorial-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace tutorial_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StreamReader reader = new StreamReader("basic-map.txt");
+            string line;
+
+
+            line = reader.ReadLine(); // read size of array
+            string size = line.Substring(1, line.Length - 2);
+
+
+            
+            
+
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] items = line.Split(' ');
+            }
+
+
+        }
+    }
+}
