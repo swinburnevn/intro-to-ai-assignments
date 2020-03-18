1cdaa35c98b923bcadd3133f320af7f68096391b
Mikanwolfe <ivory@nekox.net>
2020-03-18 20:00:47 +1100

    create assignment

---
+++ Assignment 1/robot-nagivation/robot-nagivation/Map.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace robot_nagivation
+{
+
+    public enum TileType
+    {
+        Empty,
+        Wall,
+        Start,
+        Goal
+    }
+
+    public enum AgentActions
+    {
+
+    }
+
+    /* 
+     * This is the "world" that the agent inhabits.
+     *  Contains a NxM matrix of TileType enum, which represents the tile occupying that location.
+     *  Also contains agent position.
+     * 
+     */
+     /// <summary>
+     /// Map class, contains details about the world and agent.
+     /// </summary>
+    public class Map
+    {
+        private int[] _agentPos;
+        private TileType[,] _mapMatrix;
+
+        public TileType[,] MapMatrix { get => _mapMatrix; set => _mapMatrix = value; }
+        public int[] AgentPos { get => _agentPos; set => _agentPos = value; }
+    }
+
+    /*
+     * Used to produce a map, e.g. File Parser implementation takes a file input and 
+     *  uses it to return the map.
+     */
+    public interface MapParser
+    {
+
+    }
+    public class MapFileParser : MapParser
+    {
+    }
+}
---
+++ Assignment 1/robot-nagivation/robot-nagivation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace robot_nagivation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
